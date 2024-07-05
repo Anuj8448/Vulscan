c3615be7c921d38806872a631350ebc977684d7d
Anuj8448 <92680217+Anuj8448@users.noreply.github.com>
2024-07-05 15:54:10 +0530

    Update setup.sh

--- vulscan/setup.sh
+++ vulscan/setup.sh
@@ -18,32 +18,8 @@ cd ..
 
 # Install other tools that are not available via pip
 # For example, installing nmap, dnsrecon, etc., if not already available
-apt-get install -y nmap dnsrecon wafw00f git python3-pip
+apt install nmap dnsrecon wafw00f uniscan lbd theHarvester -y
 snap install amass
 pip install sslyze
-
-# Install Uniscan
-echo "Installing uniscan..."
-git clone https://github.com/poerschke/Uniscan.git
-cd Uniscan
-chmod +x uniscan.pl
-cp uniscan.pl /usr/local/bin/uniscan
-cd ..
-
-# Install lbd (Load Balancing Detector)
-echo "Installing lbd..."
-git clone https://github.com/opsxcq/lbd.git
-cd lbd
-chmod +x lbd.py
-cp lbd.py /usr/local/bin/lbd
-cd ..
-
-# Install theHarvester
-echo "Installing theHarvester..."
-git clone https://github.com/laramies/theHarvester.git
-cd theHarvester
-pip install -r requirements.txt
-python setup.py install
-cd ..
 
 echo "All tools installed successfully."
